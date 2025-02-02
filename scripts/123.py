d11539c47c4646e39b66c87c821dacab20e839f7
lucas <lsgalante12@gmail.com>
2025-02-02 17:16:18 -0500

    attempt to fix file does not exist error

--- scripts/123.py
+++ scripts/123.py
@@ -2,14 +2,15 @@ import os.path
 
 path = "~/src/hou-ctl/recent_paths"
 path = os.path.expanduser(path)
-file = open(path, "r")
-choice_arr = file.read().split("\n")
+try:
+    file = open(path, "r")
+    choice_arr = file.read().split("\n")
 
-i = hou.ui.selectFromList(
-    choices=choice_arr,
-    exclusive=True,
-    message="recent files",
-    column_header="path"
-)
-if len(i) > 0:
-    hou.hipFile.load(choice_arr[i[0]])
+    i = hou.ui.selectFromList(
+        choices=choice_arr,
+        exclusive=True,
+        message="recent files",
+        column_header="path"
+    )
+    if len(i) > 0:
+        hou.hipFile.load(choice_arr[i[0]])
