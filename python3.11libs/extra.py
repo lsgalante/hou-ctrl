fbaba384aa82b452bd1293a0aa29789482965826
lsgalante <lsgalante12@gmail.com>
2025-02-26 08:05:29 -0500

    support for moving multiple nodes with keyboard in network editor

--- python3.11libs/extra.py
+++ python3.11libs/extra.py
@@ -1,85 +1,103 @@
 import hou, labutils, math, os
-from canvaseventtypes import *
-import nodegraphdisplay as display
-import nodegraphview as view
-from PySide2.QtCore import Qt, QRect
-from PySide2.QtGui import QColor, QCursor, QPen, QPixmap
-from PySide2.QtWidgets import QGraphicsScene, QGraphicsView
+from   canvaseventtypes  import *
+import hou_ctl_utils     as     hcu
+import nodegraphdisplay  as     display
+import nodegraphview     as     view
+from   PySide2.QtCore    import Qt, QRect
+from   PySide2.QtGui     import QColor, QCursor, QPen, QPixmap
+from   PySide2.QtWidgets import QGraphicsScene, QGraphicsView
 
 def action(uievent):
     if isinstance(uievent, ContextEvent):
         return
 
     if isinstance(uievent, KeyboardEvent) and uievent.eventtype == 'keyhit':
-        editor       = uievent.editor
-        node         = editor.currentNode()
-        x            = node.position()[0]
-        y            = node.position()[1]
-        key          = uievent.key
-        screen_size  = editor.screenBounds().size()
-        visible_size = editor.visibleBounds().size()
-        zoom_amt     = visible_size[0] / screen_size[0]
-        view_xform   = [0, 0]
-        view_step    = 160
+        editor = uievent.editor
+        key = uievent.key
 
         # Move nodes
-        if key == "Ctrl+Shift+H":
-            if round(x % 1, 1) <= 0.5: x = math.floor(x) - 0.5
-            else:                      x = math.ceil(x)  - 0.5
-        elif key == "Ctrl+Shift+J":
-            if round(y % 1, 2) > 0.85: y = math.ceil(y)  - 0.15
-            else:                      y = math.floor(y) - 0.15
-        elif key == "Ctrl+Shift+K":
-            if round(y % 1, 2) < 0.85: y = math.floor(y) + 0.85
-            else:                      y = math.ceil(y)  + 0.85
-        elif key == 'Ctrl+Shift+L':
-            if round(x % 1, 1) >= 0.5: x = math.ceil(x)  + 0.5
-            else:                      x = math.floor(x) + 0.5
+        if key in ("Ctrl+Shift+H", "Ctrl+Shift+J", "Ctrl+Shift+K", "Ctrl+Shift+L"):
+            nodes = hou.selectedNodes()
+            for node in nodes:
+                node_x = node.position()[0]
+                node_y = node.position()[1]
+
+                if key == "Ctrl+Shift+H":
+                    if round(node_x % 1, 1) <= 0.5:
+                        node_x = math.floor(node_x) - 0.5
+                    else:
+                        node_x = math.ceil(node_x) - 0.5
+
+                elif key == "Ctrl+Shift+J":
+                    if round(node_y % 1, 2) > 0.85:
+                        node_y = math.ceil(node_y) - 0.15
+                    else:
+                        node_y = math.floor(node_y) - 0.15
+
+                elif key == "Ctrl+Shift+K":
+                    if round(node_y % 1, 2) < 0.85:
+                        node_y = math.floor(node_y) + 0.85
+                    else:
+                        node_y = math.ceil(node_y) + 0.85
+
+                elif key == 'Ctrl+Shift+L':
+                    if round(node_x % 1, 1) >= 0.5:
+                        node_x = math.ceil(node_x) + 0.5
+                    else:
+                        node_x = math.floor(node_x) + 0.5
+
+                new_pos = (node_x, node_y)
+                node.setPosition(new_pos)
 
         # Organize nodes
-        elif key == "Ctrl+Shift+A":   node.parent().layoutChildren(horizontal_spacing=5, vertical_spacing=5), 
+        elif key == "Ctrl+Shift+A":
+            node = editor.currentNode()
+            node.parent().layoutChildren(horizontal_spacing=5, vertical_spacing=5), 
 
         # Place dot
         elif key == "Shift+D":
             selected = hou.selectedNodes()
             if len(selected) == 1:
                 context = node.parent()
-                dot     = context.createNetworkDot()
+                dot = context.createNetworkDot()
                 dot.setInput(node)
                 cursor_pos = editor.cursorPosition()
                 dot.setPosition(cursor_pos)
 
-        # Toggle grid mode
+        # Grid mode
         elif key == "Shift+G":
             mode = editor.getPref("gridmode")
-            modes = ("0", "2")
-            idx = (modes.index(mode) + 1) % 2
-            editor.setPref("gridmode", modes[idx]) 
+            modes = ("0", "1", "2")
+            idx = modes.index(mode)
+            idx = (idx + 1) % 3
+            mode = modes[idx]
+            editor.setPref("gridmode", mode) 
 
         # Panning
-        elif key == 'H': view_xform[0] = view_step * zoom_amt * -1
-        elif key == 'J': view_xform[1] = view_step * zoom_amt * -1
-        elif key == 'K': view_xform[1] = view_step * zoom_amt
-        elif key == 'L': view_xform[0] = view_step * zoom_amt
+        if key in ("H", "J", "K", "L"):
+            view_xform = [0, 0]
+            view_step = 160
+            screen_size = editor.screenBounds().size()
+            visible_size = editor.visibleBounds().size()
+            zoom_amt = visible_size[0] / screen_size[0]
+            if   key == "H": view_xform[0] = view_step * zoom_amt * -1
+            elif key == "J": view_xform[1] = view_step * zoom_amt * -1
+            elif key == "K": view_xform[1] = view_step * zoom_amt
+            elif key == "L": view_xform[0] = view_step * zoom_amt
+            xform = hou.Vector2(view_xform[0], view_xform[1])
+            visible_bounds = editor.visibleBounds()
+            visible_bounds.translate(xform)
+            editor.setVisibleBounds(visible_bounds)
 
-        # Toggle update mode
+        # Update mode
         elif key == "M":
-            mode  = hou.updateModeSetting()
+            mode = hou.updateModeSetting()
             modes = (hou.updateMode.Manual, hou.updateMode.AutoUpdate)
-            idx   = (modes.index(mode) + 1) % 2
+            idx = (modes.index(mode) + 1) % 2
             hou.setUpdateMode(modes[idx])
 
-        node.setPosition( (x, y) )
-        xform = hou.Vector2(view_xform[0], view_xform[1])
-        visible_bounds = editor.visibleBounds()
-        visible_bounds.translate(xform)
-        editor.setVisibleBounds(visible_bounds)
 
-        # Draw outline
-#        draw_outline(uievent)
-
-
-def draw_outline(uievent):
+def drawOutline(uievent):
     editor = uievent.editor
     image = hou.NetworkImage()
     image.setPath('$HIP/drawings/bg.png')
