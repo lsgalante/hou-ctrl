e3af4468cffcd7b054fab87efc32addd5e9b6599
lsgalante <lsgalante12@gmail.com>
2025-02-28 10:13:56 -0500

    make startup recent files scripts work with python posixpath

--- scripts/123.py
+++ scripts/123.py
@@ -1,31 +1,23 @@
-import os.path
 import pathlib
 
 path_raw = "~/src/hou-ctl/recent_paths"
-path = PosixPath(path_raw)
+path = pathlib.PosixPath(path_raw)
 path = path.expanduser()
 
-if path.is_file():
-    continue
-
-else:
+if not path.is_file():
     path.touch()
 
-choices_raw = file.read_text()
+choices_raw = path.read_text()
 choice_arr = choices_raw.split("\n")
 
 i = hou.ui.selectFromList(
     choices=choice_arr,
     exclusive=True,
-    message="recent files",
-    column_header="path"
+    message="Recent Files",
+    column_header="Path"
 )
 
 # on accept
 if len(i) > 0:
     choice = choice_arr[i[0]]
     hou.hipFile.load(choice)
-
-# on cancel
-else:
-    return
