9d9fc646ffc59670a8812b720e0bbeedb2f3d3c7
lucas <lsgalante12@gmail.com>
2025-02-27 19:46:21 -0500

    begin improving startup code

--- scripts/123.py
+++ scripts/123.py
@@ -1,18 +1,31 @@
 import os.path
+import pathlib
 
-path = "~/src/hou-ctl/recent_paths"
-path = os.path.expanduser(path)
-try:
-    file = open(path, "r")
-    choice_arr = file.read().split("\n")
+path_raw = "~/src/hou-ctl/recent_paths"
+path = PosixPath(path_raw)
+path = path.expanduser()
 
-    i = hou.ui.selectFromList(
-        choices=choice_arr,
-        exclusive=True,
-        message="recent files",
-        column_header="path"
-    )
-    if len(i) > 0:
-        hou.hipFile.load(choice_arr[i[0]])
-except:
-    x=1
+if path.is_file():
+    continue
+
+else:
+    path.touch()
+
+choices_raw = file.read_text()
+choice_arr = choices_raw.split("\n")
+
+i = hou.ui.selectFromList(
+    choices=choice_arr,
+    exclusive=True,
+    message="recent files",
+    column_header="path"
+)
+
+# on accept
+if len(i) > 0:
+    choice = choice_arr[i[0]]
+    hou.hipFile.load(choice)
+
+# on cancel
+else:
+    return
