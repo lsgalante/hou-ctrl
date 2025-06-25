9acfb618f351fb89a6c65542815f76fcf009f90d
lsgalante <lsgalante12@gmail.com>
2025-06-25 10:27:02 -0400

    backpedal some keybindings

--- scripts/python/hctl_bindings.py
+++ scripts/python/hctl_bindings.py
@@ -1,14 +1,12 @@
 import hou
 
+# Key names:
 # uparrow, downarrow, leftarrow, rightarrow
 # alt, cmd, shift, ctrl, tab, del, backspace, enter, esc
 # f1, f2...
 # pageup, pagedown, end, home, insert
 # /, \\
 # pad0, pad1, pad2...
-# ref:h?h.cut
-# ref:h.pane.gview.model.sel?h.pane.gview.model.sel.loop
-# ref:h?h.handleeditor
 # padstar, padslash
 # pause
 # space
@@ -16,7 +14,7 @@ import hou
 def updateBindings():
     hou.hotkeys.revertToDefaults("h", "", False)
 
-    # clear assignments
+    # Clear all assignments from command
     assignment_clear_arr = (
         "deskmgr.new",
         "deskmgr.save",
@@ -30,77 +28,71 @@ def updateBindings():
         "h.clear_cop_caches",
         "h.comp_project_manager",
         "h.context_help",
-        "h.copy",
-        "h.cut",
+        # "h.copy",
+        # "h.cut",
         "h.desktop_mgr",
-        "h.find",
+        "h.desktopToggleHctl"
+        # "h.find",
         "h.mat_palette",
-        "h.open",
-        "h.pane.closetab",
-        "h.paste",
-        "h.redo",
-        "h.save",
-        "h.undo",
+        # "h.open",
+        # "h.pane.closetab",
+        # "h.paste",
+        # "h.redo",
+        # "h.save",
+        # "h.undo",
 
         "h.pane.gview.decrease_subd",
         "h.pane.gview.increase_subd",
 
-        "h.pane.edit_bookmark",
-        "h.pane.nexttab",
-        "h.pane.to_parm",
+        # "h.pane.edit_bookmark",
+        # "h.pane.nexttab",
+        # "h.pane.to_parm",
 
+        "h.pane.wsheet.add_image",
+        "h.pane.wsheet.add_postit",
+        "h.pane.wsheet.addfileop",
+        "h.pane.wsheet.allowdroponwire",
         "h.pane.wsheet.batch_rename",
-        "h.pane.wsheet.stitch_mode",
-        "h.pane.wsheet.up",
         "h.pane.wsheet.down",
+        "h.pane.wsheet.edit_images",
+        "h.pane.wsheet.layout", "l",
+        "h.pane.wsheet.layout_all",
         "h.pane.wsheet.left",
+        "h.pane.wsheet.maximize",
+        "h.pane.wsheet.minimize",
         "h.pane.wsheet.node_quick_nav",
         "h.pane.wsheet.right",
+        "h.pane.wsheet.scope_chans",
         "h.pane.wsheet.select",
+        "h.pane.wsheet.stitch_mode",
+        "h.pane.wsheet.up",
         "h.pane.wsheet.zoom_in",
         "h.pane.wsheet.zoom_out",
 
         "inputfield.context_help"
     )
-        
-    # remove assignments
+
+    # Remove assignments
     assignment_remove_arr = (
-        ("h.desktopToggleHctl", "cmd+p"),
-        
-        ("h.pane.wsheet.add_image", "shift+i"),
-        ("h.pane.wsheet.addfileop", "="),
-        ("h.pane.wsheet.add_postit", "shift+p"),
-        ("h.pane.wsheet.scope_chans", "v"),
-        ("h.pane.wsheet.allowdroponwire", "cmd+8"),
-        ("h.pane.wsheet.allowdroponwire", "cmd+pad8"),
-        ("h.pane.wsheet.edit_images", "cmd+i"),
+        ("h.cut", "alt+x"),
         ("h.pane.wsheet.home", "h"),
         ("h.pane.wsheet.home_selected", "shift+h"),
-        ("h.pane.wsheet.layout_all", "shift+l"),
-        ("h.pane.wsheet.layout", "l"),
-        ("h.pane.wsheet.maximize", "shift+k"),
-        ("h.pane.wsheet.minimize", "shift+j"),
-
-        ("h.pane.gview.decrease_subd" "shift+-"),
-        ("h.pane.gview.increase_subd" "shift+="),
-        
-        ("h.pane.gview.state.volatile_chmodify", "j")
     )
 
-    # add assignments
+    # New assignments
     assignment_add_arr = (
-        ("deskmgr.save", "ctrl+s"),
-        
-        ("h.copy", "cmd+w"),
-        ("h.cut", "ctrl+w"),
-        ("h.desktopToggleHctl", "cmd+x"),
+        # ("deskmgr.save", "ctrl+s"),
+
+        # ("h.copy", "cmd+w"),
+        # ("h.cut", "ctrl+w"),
+        ("h.desktopToggleHctl", "alt+x"),
         # ("h.find", "ctrl+s"),
-        ("h.open", "ctrl+o"),
-        ("h.paste", "ctrl+y"),
-        ("h.redo", "ctrl+shift+z"),
-        ("h.save", "cmd+s"),
+        # ("h.open", "ctrl+o"),
+        # ("h.paste", "ctrl+y"),
+        # ("h.redo", "ctrl+shift+z"),
+        # ("h.save", "cmd+s"),
         ("h.sceneViewerToggleKeycam", "shift+k"),
-        ("h.undo", "ctrl+/"),
+        # ("h.undo", "ctrl+/"),
 
         ("h.pane.editparms.selectall", "alt+h"),
 
@@ -119,7 +111,7 @@ def updateBindings():
         ("h.pane.wsheet.select", "ctrl+alt+h"),
         # ("h.pane.wsheet.zoom_in", "="),
         # ("h.pane.wsheet.zoom_out", "-"),
-        
+
         ("h.pane.textport.down", "ctrl+n"),
         ("h.pane.textport.up", "ctrl+p"),
         ("h.pane.textport.bottom", "ctrl+e"),
@@ -152,12 +144,12 @@ def updateBindings():
         ("inputfield.up", "ctrl+p")
     )
 
-    # command bindings
-    # command_binding_arr = (
+    # Command bindings
+    command_binding_arr = (
         # ("h.pane.pythonshell", "h.desktopToggleHctl")
-    # )
+    )
 
-    
+
     for assignment in assignment_clear_arr:
         context = assignment.rpartition(".")[0]
         symbol = assignment
@@ -175,8 +167,8 @@ def updateBindings():
         symbol = assignment[0]
         key = assignment[1]
         hou.hotkeys.addAssignment(context, symbol, key)
-    
-    # for binding in command_binding_arr:
-        # context = binding[0]
-        # command = binding[1]
-        # hou.hotkeys.addCommandBinding(context, command)
+
+    for binding in command_binding_arr:
+        context = binding[0]
+        command = binding[1]
+        hou.hotkeys.addCommandBinding(context, command)
