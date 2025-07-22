f12d32c24713b12f58acb63e229de02f222a557c
lsgalante <lsgalante12@gmail.com>
2025-07-22 10:25:33 -0400

    Apparently abbreviate package imports

--- python3.11libs/nodegraphhooks.py
+++ python3.11libs/nodegraphhooks.py
@@ -1,16 +1,15 @@
 import hou
 from canvaseventtypes import *
 import nodegraphdisplay as display
-import nodegraphview as view
-
+# import nodegraphview as view
 import math
-
 from importlib import reload
-import nodegraphfunctions as ngf
+import nodegraphmain as ngm
+
 
 def createEventHandler(uievent, pending_actions):
-    reload(ngf)
-    if ngf.action(uievent):
+    reload(ngm)
+    if ngm.action(uievent):
         return None, True
     else:
         return None, False
