1f05c51cd8f8c31690d18a9f6e04c0646b30b6ab
lsgalante <lsgalante12@gmail.com>
2025-11-14 14:39:13 -0500

    format bindings

--- python3.11libs/hctl/utils/hcbindings.py
+++ python3.11libs/hctl/utils/hcbindings.py
@@ -13,11 +13,21 @@ import hou
 
 
 def load():
+
+    # Macos or linux
+    if sys == "Darwin":
+        modifier = Qt.MetaModifier
+    elif sys == "linux":
     hou.hotkeys.revertToDefaults("h", "", False)
-    # Clear all assignments from command
-    assignment_clear_arr = (
+
+    assignments_to_clear = (
+        #
+        # Desktop manager
         "deskmgr.new",
         "deskmgr.save",
+        "h.desktop_mgr",
+        #
+        # Global
         "h.add_key",
         "h.aliases",
         "h.audio_panel",
@@ -27,89 +37,72 @@ def load():
         "h.clear_cop_caches",
         "h.comp_project_manager",
         "h.context_help",
-        # "h.copy",
-        # "h.cut",
-        "h.desktop_mgr",
-        "h.desktopToggleHctl",
-        # "h.find",
         "h.mat_palette",
-        # "h.open",
-        # "h.pane.closetab",
-        # "h.paste",
-        # "h.redo",
-        # "h.save",
-        # "h.undo",
-        "h.pane.gview.decrease_subd",
+        #
+        # hctl
+        "h.desktopToggleHctl",
+        #
+        # Pane
+        #
+        # Pane - scene viewer
         "h.pane.gview.increase_subd",
+        "h.pane.gview.decrease_subd",
         "h.pane.gview.restore_prevcam",
         "h.pane.gview.state.volatile_chmodify",
-        # "h.pane.edit_bookmark",
-        # "h.pane.nexttab",
-        # "h.pane.to_parm",
+        #
+        # Pane - network editor
         "h.pane.wsheet.add_image",
         "h.pane.wsheet.add_postit",
         "h.pane.wsheet.addfileop",
         "h.pane.wsheet.allowdroponwire",
         "h.pane.wsheet.batch_rename",
+        "h.pane.wsheet.up",
         "h.pane.wsheet.down",
+        "h.pane.wsheet.left",
+        "h.pane.wsheet.right",
         "h.pane.wsheet.edit_images",
         "h.pane.wsheet.layout",
-        "l",
         "h.pane.wsheet.layout_all",
-        "h.pane.wsheet.left",
         "h.pane.wsheet.maximize",
         "h.pane.wsheet.minimize",
         "h.pane.wsheet.node_quick_nav",
-        "h.pane.wsheet.right",
         "h.pane.wsheet.scope_chans",
         "h.pane.wsheet.select",
         "h.pane.wsheet.stitch_mode",
-        "h.pane.wsheet.up",
         "h.pane.wsheet.zoom_in",
         "h.pane.wsheet.zoom_out",
+        #
+        # Input fields
         "inputfield.context_help",
     )
 
-    # Remove assignments
-    assignment_remove_arr = (
+    assignments_to_remove = (
+        #
+        # Global
         ("h.cut", "alt+x"),
+        #
+        # Network editor
         ("h.pane.wsheet.home", "h"),
         ("h.pane.wsheet.home_selected", "shift+h"),
     )
 
-    # New assignments
-    assignment_add_arr = (
-        # Desktop
-        # ("deskmgr.save", "ctrl+s"),
+    assignments_to_add = (
+        # Pane
         ("h.pane.shelf", "ctrl+]"),
-        # ("h.reset_shelf", "ctrl+["),
+        ("h.pane.shelf", "cmd+]"),
+        ("h.pane.nexttab", "ctrl+space"),
+        ("h.pane.prevtab", "ctrl+shift+space"),
         #
-        # hc
+        # hctl
         ("h.hCSessionPanel", "alt+x"),
         ("h.hCSessionPanel", "\\"),
-        #
-        # Generic
-        # ("h.copy", "cmd+w"),
-        # ("h.cut", "ctrl+w"),
-        # ("h.find", "ctrl+s"),
-        # ("h.open", "ctrl+o"),
-        # ("h.paste", "ctrl+y"),
-        # ("h.redo", "ctrl+shift+z"),
-        # ("h.save", "cmd+s"),
-        # ("h.hCSessionKeycam", "shift+k"),
-        # ("h.undo", "ctrl+/"),
-        #
-        ("h.pane.editparms.selectall", "alt+h"),
         #
         # Scene viewer
         ("h.pane.gview.refplane", "shift+g"),
         #
-        # Pane tab
-        ("h.pane.nexttab", "ctrl+space"),
-        ("h.pane.prevtab", "ctrl+shift+space"),
-        #
-        # Parameter
+        # Parameters
         ("h.pane.parms.edit_expression", "ctrl+e"),
+        ("h.pane.editparms.selectall", "alt+h"),
         #
         # Network editor vim
         # ("h.pane.wsheet.up", "shift+k"),
@@ -118,32 +111,32 @@ def load():
         # ("h.pane.wsheet.right", "shift+l"),
         ("h.pane.wsheet.node_quick_nav", "ctrl+s"),
         ("h.pane.wsheet.select", "ctrl+alt+h"),
-        # ("h.pane.wsheet.zoom_in",        "="),
-        # ("h.pane.wsheet.zoom_out",       "-"),
+        # ("h.pane.wsheet.zoom_in", "="),
+        # ("h.pane.wsheet.zoom_out", "-"),
         #
         # Text editor emacs
-        # ("h.pane.textport.up",             "ctrl+p"),
-        # ("h.pane.textport.down",           "ctrl+n"),
-        # ("h.pane.textport.next",           "ctrl+f"),
-        # ("h.pane.textport.prev",           "ctrl+b"),
-        # ("h.pane.textport.top",            "ctrl+a"),
-        # ("h.pane.textport.bottom",         "ctrl+e"),
-        # ("h.pane.textport.nextword",       "cmd+f"),
-        # ("h.pane.textport.pageup",         "cmd+v"),
-        # ("h.pane.textport.pagedown",       "ctrl+v"),
-        # ("h.pane.textport.firstline",      "cmd+shift+,"),
-        # ("h.pane.textport.lastline",       "cmd+shift+."),
-        # ("h.pane.textport.jump_to_line",   "alt+g"),
-        # ("h.pane.textport.selectup",       "ctrl+shift+p"),
-        # ("h.pane.textport.selectdown",     "ctrl+shift+n"),
-        # ("h.pane.textport.selectnext",     "ctrl+shift+f"),
-        # ("h.pane.textport.selectprev",     "ctrl+shift+b"),
-        # ("h.pane.textport.selectbottom",   "ctrl+shift+e"),
-        # ("h.pane.textport.selecttop",      "ctrl+shift+a"),
+        # ("h.pane.textport.up", "ctrl+p"),
+        # ("h.pane.textport.down", "ctrl+n"),
+        # ("h.pane.textport.next", "ctrl+f"),
+        # ("h.pane.textport.prev", "ctrl+b"),
+        # ("h.pane.textport.top", "ctrl+a"),
+        # ("h.pane.textport.bottom", "ctrl+e"),
+        # ("h.pane.textport.nextword", "cmd+f"),
+        # ("h.pane.textport.pageup", "cmd+v"),
+        # ("h.pane.textport.pagedown", "ctrl+v"),
+        # ("h.pane.textport.firstline", "cmd+shift+,"),
+        # ("h.pane.textport.lastline", "cmd+shift+."),
+        # ("h.pane.textport.jump_to_line", "alt+g"),
+        # ("h.pane.textport.selectup", "ctrl+shift+p"),
+        # ("h.pane.textport.selectdown", "ctrl+shift+n"),
+        # ("h.pane.textport.selectnext", "ctrl+shift+f"),
+        # ("h.pane.textport.selectprev", "ctrl+shift+b"),
+        # ("h.pane.textport.selectbottom", "ctrl+shift+e"),
+        # ("h.pane.textport.selecttop", "ctrl+shift+a"),
         # ("h.pane.textport.selectnextword", "cmd+shift+f"),
         # ("h.pane.textport.toggle_comment", "ctrl+;"),
         #
-        # Text editor
+        # Text editor vim
         ("h.pane.textport.up", "ctrl+k"),
         ("h.pane.textport.down", "ctrl+j"),
         ("h.pane.textport.next", "ctrl+l"),
@@ -177,29 +170,29 @@ def load():
     )
 
     # Command bindings
-    command_binding_arr = (
+    commands_to_bind = (
         # ("h.pane.pythonshell", "h.desktopToggleHctl")
     )
 
-    for assignment in assignment_clear_arr:
+    for assignment in assignments_to_clear:
         context = assignment.rpartition(".")[0]
         symbol = assignment
         hou.hotkeys.clearAssignments(context, symbol)
 
-    for assignment in assignment_remove_arr:
+    for assignment in assignments_to_remove:
         context = assignment[0].rpartition(".")[0]
         symbol = assignment[0]
         key = assignment[1]
         hou.hotkeys.removeAssignment(context, symbol, key)
 
-    for assignment in assignment_add_arr:
+    for assignment in assignments_to_add:
         context = assignment[0].rpartition(".")[0]
         print(context)
         symbol = assignment[0]
         key = assignment[1]
         hou.hotkeys.addAssignment(context, symbol, key)
 
-    for binding in command_binding_arr:
+    for binding in commands_to_bind:
         context = binding[0]
         command = binding[1]
         hou.hotkeys.addCommandBinding(context, command)
