5cfad44dd00dc1a1a0b380a0dc78a2115c1d9428
lsgalante <lsgalante12@gmail.com>
2025-11-17 12:48:28 -0500

    add new classes to api

--- python3.11libs/hctl/__init__.py
+++ python3.11libs/hctl/__init__.py
@@ -1,4 +1,13 @@
+from .core import hcglobal, hcpane, hctab
 from .ui import hcpanel, hcstatuspanel
 from .utils import hcbindings, hclistener, hcutils
 
-__all__ = ["hcpanel", "hcstatuspanel", "hcbindings", "hclistener", "hcutils"]
+__all__ = [
+    "hcglobal",
+    "hcpane",
+    "hcpanel",
+    "hcstatuspanel",
+    "hcbindings",
+    "hclistener",
+    "hcutils"
+]
